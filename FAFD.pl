a63d81193ebe9bcd730fd3f585073113066b0b2d
thingywhat <thingywhat@gmail.com>
2018-08-15 10:39:28 -0600

    Clean up whitespace and a few non-code typos

--- FAFD.pl
+++ FAFD.pl
@@ -21,11 +21,11 @@ print "FurAffinity Favorites Downloader version $version\n"
     . "By thingywhat (https://github.com/thingywhat)\n\n"
     . "\t-h\t\t\tThis help screen.\n"
     . "\t-u <user> \t\tThe user to work with. (Required)\n"
-    . "\t-d [folder]\t\tWhere to download the files.\n\n"
+    . "\t-d [folder]\t\tWhere to download the files.\n"
     . "\t-v\t\t\tMake downloads verbose.\n\n"
     . "For example, to download the favorites of thingywhat we could:\n\n"
     . "\t$0 -u thingywhat\n\n"
-    . "Or to specify a palce for them to be downloaded:\n\n"
+    . "Or to specify a place for them to be downloaded:\n\n"
     . "\t$0 -u thingywhat -d ~/Downloads/FA\n\n"
     and exit if defined $args{h} or not defined $args{u};
 
@@ -36,7 +36,7 @@ my $user = $args{u};
 my $favoriteUrl = "http://www.furaffinity.net/favorites/$user";
 
 # Make the download folder if it doesn't exist already
-mkdir($folder, 0755) unless(-d $folder );
+mkdir($folder, 0755) unless(-d $folder);
 
 # Returns a list of image IDs that are the favorites of the user
 # this script is downloading for.
@@ -46,17 +46,17 @@ sub getFavorites{
     my @favorites;
 
     do {
-	my @currentPage = ();
-	my $content = get("$favoriteUrl/$page");
-	
-	while ($content =~ /\/view\/(\d+)/g){
-	    push(@currentPage, $1);
-	}
-	if(@currentPage){
-	    push(@favorites, @currentPage);
-	} else {
-	    return @favorites;
-	}
+        my @currentPage = ();
+        my $content = get("$favoriteUrl/$page");
+
+        while ($content =~ /\/view\/(\d+)/g){
+            push(@currentPage, $1);
+        }
+        if(@currentPage){
+            push(@favorites, @currentPage);
+        } else {
+            return @favorites;
+        }
     } while($page++);
 }
 
@@ -67,15 +67,16 @@ sub download($){
 
     $content =~ /<a href="\/\/([^"]+)">\s*Download\s*<\/a>/;
     my $url = $1;
-    
+
     $url =~ /\/([^\/]+)$/;
     my $filename = $1;
-    
+
     if($verbose) {
-	print "Downloading $url to $folder/$filename!\n";
+        print "Downloading $url to $folder/$filename!\n";
     } else {
-	print "Fetching $filename\n";
+        print "Fetching $filename\n";
     }
+
     getstore("http://$url", "$folder/$filename");
 }
 
